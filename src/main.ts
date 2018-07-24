bc6ca8faee4176cd85e17c762910cecb0cd9dc14
vic lotana <viclotana@gmail.com>
2018-07-24 13:26:30 +0100

    err6

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { ASTWithSource } from '@angular/compiler';
 
 
 if (environment.production) {
   enableProdMode();
 }
+twis
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
