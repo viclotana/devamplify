ed8fe3acff5a37497e9be57dcb65f41ebc235709
vic lotana <viclotana@gmail.com>
2018-07-24 13:26:53 +0100

    updates

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-w
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
